036453a34710ae4c95b6483a2d7a6e966928fd8c
Rob Wells <rob@robjwells.com>
2021-04-14 12:31:22 +0100

    Tidy up process.py

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import json
+from typing import cast, Iterator
 
-import pandas as pd
-import pendulum
+import pandas as pd  # type: ignore
+from pendulum.datetime import DateTime
+from pendulum.parser import parse
 
 
 def parse_line(line: list[str]) -> tuple[int, str, str] | None:
@@ -19,20 +21,20 @@ def parse_line(line: list[str]) -> tuple[int, str, str] | None:
         return None
 
 
-def load_file(year, month):
+def load_file(year: int, month: int) -> str:
     filename = f"sunrise-{year}-{month:02}.html"
     with open(filename) as f:
         data = f.read()
     return data
 
 
-def data_to_table(data):
+def data_to_table(data: str) -> pd.DataFrame:
     t = pd.read_html(data, attrs={'id': 'as-monthsun'})[0]
     subset = t[t.keys()[:3]]
     return subset
 
 
-def date_lines_from_table(table):
+def date_lines_from_table(table: pd.DataFrame) -> list[tuple[int, str, str]]:
     lines = table.values
     maybe_parsed = [parse_line(line) for line in lines]
     return [parsed for parsed in maybe_parsed if parsed is not None]
@@ -48,10 +50,11 @@ def process_lines(year: int, month: int) -> list[tuple[int, str, str]]:
 def parse_date(
     year: int, month: int, day: int, time: str
 ) -> str:
-    return pendulum.parse(
+    date = parse(
         f"{year}-{month:02}-{day:02}T{time}",
         tz="Europe/London",
-    ).to_iso8601_string()
+    )
+    return date.to_iso8601_string()  # type: ignore
 
 
 def dates_to_dict(day: str, sunrise: str, sunset: str) -> dict[str, str]:
@@ -62,23 +65,26 @@ def dates_to_dict(day: str, sunrise: str, sunset: str) -> dict[str, str]:
     }
 
 
-def do_thing():
-    for year in (2021, 2022):
+def get_month(year: int, month: int) -> Iterator[dict[str, str]]:
+    lines = process_lines(year, month)
+    for line in lines:
+        day, sunrise, sunset = line
+        date_dict = dates_to_dict(
+            f"{year:02}-{month:02}-{day:02}",
+            parse_date(year, month, day, sunrise),
+            parse_date(year, month, day, sunset),
+        )
+        yield date_dict
+
+
+def get_all_months(years: list[int]) -> Iterator[dict[str, str]]:
+    for year in years:
         for month in range(1, 13):
-            lines = process_lines(year, month)
-            for line in lines:
-                day, sunrise, sunset = line
-                date_dict = dates_to_dict(
-                    f"{year:02}-{month:02}-{day:02}",
-                    parse_date(year, month, day, sunrise),
-                    parse_date(year, month, day, sunset),
-                )
-                yield date_dict
+            yield from get_month(year, month)
 
-
-def main():
+def main() -> None:
     print(
-        json.dumps(list(do_thing()))
+        json.dumps(list(get_all_months([2021, 2022])))
     )
 
 
