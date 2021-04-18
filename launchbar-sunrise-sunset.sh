73d17701eecc1b09abd23f770a4e188d6cd1eadf
Rob Wells <rob@robjwells.com>
2021-04-18 13:53:16 +0100

    Enhance LaunchBar action with pure SQL

--- launchbar-sunrise-sunset.sh
+++ launchbar-sunrise-sunset.sh
@@ -2,40 +2,82 @@
 
 set -euo pipefail
 
-PATH="/usr/local/bin:$PATH"
 DBFILE="/Users/robjwells/projects/sunrise-sunset/sunrise-sunset-times.db"
 
 function query() {
-	sqlite3 -cmd '.mode lines' $DBFILE <<- EOF
+	sqlite3 $DBFILE <<- EOF
+		with basic as (
+			select
+				strftime('%H:%M', sunrise, 'localtime') as sunrise,
+				strftime('%H:%M', sunset, 'localtime') as sunset,
+				strftime('%s', sunrise) - strftime('%s', 'now') <= 0 as sunrise_past,
+				strftime('%s', sunset) - strftime('%s', 'now') <= 0 as sunset_past,
+				abs(strftime('%s', sunrise) - strftime('%s', 'now')) as sunrise_diff,
+				abs(strftime('%s', sunset) - strftime('%s', 'now')) as sunset_diff
+			from
+				london
+			where
+				date = date('now')
+		),
+		parts as (
+			select
+				*,
+				ltrim(strftime('%H', sunrise_diff, 'unixepoch'), '0') as sunrise_hours,
+				ltrim(strftime('%M', sunrise_diff, 'unixepoch'), '0') as sunrise_minutes,
+				ltrim(strftime('%H', sunset_diff, 'unixepoch'), '0') as sunset_hours,
+				ltrim(strftime('%M', sunset_diff, 'unixepoch'), '0') as sunset_minutes
+			from
+				basic
+		),
+		formatted as (
+			select
+				*,
+				sunrise_hours || ' hours ' || sunrise_minutes || ' minutes' as sunrise_formatted,
+				sunset_hours || ' hours ' || sunset_minutes || ' minutes' as sunset_formatted
+			from
+				parts
+		),
+		relative as (
+			select
+				*,
+				case when sunrise_past then
+					sunrise_formatted || ' ago'
+				else
+					'in ' || sunrise_formatted
+				end as sunrise_relative,
+				case when sunset_past then
+					sunset_formatted || ' ago'
+				else
+					'in ' || sunset_formatted
+				end as sunset_relative
+			from 
+				formatted
+		), processed as (
+			select
+				sunrise,
+				sunrise_relative,
+				sunset,
+				sunset_relative
+			from
+				relative
+		)
 		select
-			strftime('%H:%M', sunrise, 'localtime') as sunrise,
-			strftime('%H:%M', sunset, 'localtime') as sunset
+			json_array(
+				json_object(
+					'title', sunrise_relative,
+					'badge', sunrise,
+					'icon', 'font-awesome:fa-sun-o'
+				),
+				json_object(
+					'title', sunset_relative,
+					'badge', sunset,
+					'icon', 'font-awesome:fa-moon-o'
+				)
+			) as launchbar_items
 		from
-			london
-		where
-			date = date('now')
+			processed
 		;
 	EOF
 }
 
-function sunrise_end() {
-	sd '(sunrise.+)$' '$1", "icon": "font-awesome:fa-sun-o"}'
-}
-
-function sunset_end() {
-	sd '(sunset.+)$' '$1", "icon": "font-awesome:fa-moon-o"}'
-}
-
-function add_front() {
-	sd '^\s*sun\w+ = ' '{"title": "'
-}
-
-function add_end() {
-	sunrise_end | sunset_end
-}
-
-function format_object() {
-	add_end | add_front
-}
-
-query | format_object | jq --slurp
+query
